227a79d09afc27ae9b0153c846e3bfb0928e36e0
wfhKing <1124541443@qq.com>
2018-08-07 09:51:06 +0800

    yes

---
+++ Demo03/src/com/HelloWorld.java
@@ -0,0 +1,35 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorld",urlPatterns = {"/HelloWorld"})
+//用了urlPatterns就不用配置xml文件dd
+public class HelloWorld extends HttpServlet {
+    private String message;
+
+    public void init() throws ServletException
+    {
+        // 初始化
+        message = "Hello, First Servlet!";
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置:响应内容类型
+        response.setContentType("text/html");
+
+        // 输出文本
+        PrintWriter out = response.getWriter();
+        out.write("<h1> " + message + " </h1>");
+    }
+
+}
